7f52566e2ed634ca3b3fc7a01cda8977ae210144
Geoff Smith <smith260194@gmail.com>
2019-05-16 16:00:06 +0100

    Doing more SRP stuff

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,20 +1,43 @@
+require 'birthday'
+
 class Birthday_list
   def initialize
     @birthdays = []
   end
+
   def store_birthday(argument)
     @birthdays.push(Birthday.new(argument))
   end
+
   def show_birthdays
     @birthdays.each do |birthday|
       puts "#{birthday.name} => #{birthday.date.strftime('%d %b %Y')}"
     end
   end
+
   def whose_birthday
     @birthdays.each do |birthday|
-      if birthday.date.month == Time.now.month && birthday.date.day == Time.now.day
-        puts "It's #{birthday.name}'s birthday today! They are #{Time.now.year - birthday.date.year} years old!"
+      if birthday_is_today?(birthday)
+        print(birthday)
       end
     end
+  end
+
+  private
+
+  def print(birthday)
+    puts "It's #{birthday.name}'s birthday today! They are #{Time.now.year - birthday.date.year} years old!"
+  end
+
+  def birthday_is_today?(birthday)
+    month_of_birth?(birthday) && day_of_birth?(birthday)
+  end
+
+  def day_of_birth?(birthday)
+    birthday.date.day == Time.now.day
+  end
+
+  def month_of_birth?(birthday)
+    birthday.date.month == Time.now.month
   end
 end
